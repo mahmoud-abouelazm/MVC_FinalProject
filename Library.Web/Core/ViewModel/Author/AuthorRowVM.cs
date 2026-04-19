8f770e8daf64d96901ce66aa7a6bbf1f5bf2235d
moshady21 <moshady21@gmail.com>
2026-04-19 15:52:49 +0200

    Finish Authors Page

--- Library.Web/Core/ViewModel/Author/AuthorRowVM.cs
+++ Library.Web/Core/ViewModel/Author/AuthorRowVM.cs
@@ -1,4 +1,8 @@
-﻿namespace Library.Web.Core.ViewModel.Author
+﻿using Library.Web.Core.Models;
+using Library.Web.Core.ViewModel.Book;
+
+
+namespace Library.Web.Core.ViewModel.Author
 {
     public class AuthorRowVM
     {
@@ -6,5 +10,7 @@
         public string Name { get; set; } = null!;
         public string? Bio { get; set; }
         public int BookCount { get; set; }
+
+        public List<BookRowVM>? BooksOfTheAuthor { get; set; }    
     }
 }
